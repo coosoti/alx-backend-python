2141f9bd0b49ee16c90afa30c5a4b06e61ab93f8
coosoti <charlesosoti@gmail.com>
2022-01-01 07:10:47 +0300

    added TestMemoize(unittest.TestCase) class with a test_memoize method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ import requests
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,3 +49,21 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get') as mock_req:
             mock_req.return_value.json.return_value = payload
             self.assertEqual(get_json(url), payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test class for memoize function"""
+    class TestClass:
+
+        def a_method(self):
+            return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    with patch.object(TestClass, 'a_method') as mock:
+        test = TestClass()
+        test.a_property()
+        test.a_property()
+        mock.assert_called_once()
