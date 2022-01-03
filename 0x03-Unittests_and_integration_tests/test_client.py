e6f742d9826c93a23d295d27c98c8692a245ce63
coosoti <charlesosoti@gmail.com>
2022-01-03 14:01:30 +0300

    added TestGithubOrgClient.test_public_repos to unit-test GithubOrgClient.public_repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 this module contains unittests for client
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import PropertyMock, patch
 from client import GithubOrgClient
 from parameterized import param, parameterized
 
@@ -22,3 +22,47 @@ class TestGithubOrgClient(unittest.TestCase):
         correct value"""
         GithubOrgClient(org_name).org
         mock_getJson.assert_called_once_with(resource)
+
+    def test_public_repos_url(self):
+        """this method test the public repos url
+        should return a known payload
+        """
+        with patch.object(
+                GithubOrgClient, 'org', new_callable=PropertyMock
+        ) as org_mock:
+            org_mock.return_value = {
+                "repos_url":  'https://api.github.com/orgs/google/repos'
+            }
+            res = GithubOrgClient('google').org["repos_url"]
+            self.assertEqual(res, 'https://api.github.com/orgs/google/repos')
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_getJson):
+        """this method unit-test GithubOrgClient.public_repos"""
+        test = [
+            {'id': 1, 'name': 'Docker'},
+            {'id': 2, 'name': 'Kubernetes'},
+            {'id': 2, 'name': 'Vagrant'}
+        ]
+        mock_getJson.return_value = test
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as public_repos_url_mock:
+            public_repos_url_mock.return_value = 'https://github.com/org/abc'
+            client0 = GithubOrgClient('abc')
+            expected = ['Docker', 'Kubernetes', 'Vagrant']
+            self.assertEqual(client0.public_repos(), expected)
+            public_repos_url_mock.assert_called_once()
+        mock_getJson.assert_called_once()
+
+        @parameterized.expand([
+            param(repo={"license": {"key": "my_license"}},
+                  license_key="my_license", expected=True),
+            param(repo={"license": {"key": "other_license"}},
+                  license_key="my_license", expected=False)
+        ])
+        def test_has_license(self, repo, license_key, expected):
+            """
+            this method tests the has_license function of the Client class
+            """
+            has_key_bool = GithubOrgClient.has_license(repo, license_key)
+            self.assertEqual(has_key_bool, expected)
